1ef8bdf0fbcf3a89353da3b21f2f39eac8f51dfd
Le William <lewilli1@teach.cs.toronto.edu>
2017-11-07 16:50:24 -0500

    base q4

--- table_definitions/q4.sql
+++ table_definitions/q4.sql
@@ -17,11 +17,39 @@ CREATE TABLE q4(
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS parties_and_policy CASCADE;
 
 -- Define views for your intermediate steps here.
+CREATE VIEW parties_and_policy AS (
+SELECT p.name, c.name as countryName, pp.left_right
+FROM party p 
+	left join party_position pp  on p.id = pp.party_id 
+	left join country c on p.country_id = c.id
 
+);
 
 -- the answer to the query 
-INSERT INTO q4 
+--INSERT INTO q4 
+INSERT into q4 
+Select p.countryName, count(*) as r0_2, Null as r2_4, Null as r4_6, Null as r6_8
+FROM parties_and_policy p
+WHERE  0 <= p.left_right  and p.left_right < 2
+Group by p.countryName
 
+INSERT into q4 
+Select p.countryName, Null as r0_2, count(*) as r2_4, Null as r4_6, Null as r6_8
+FROM parties_and_policy p
+WHERE  2 <= p.left_right  and p.left_right < 4
+Group by p.countryName
+
+INSERT into q4 
+Select p.countryName, Null as r0_2, Null as r2_4, count(*) as r4_6, Null as r6_8
+FROM parties_and_policy p
+WHERE  4 <= p.left_right  and p.left_right < 6
+Group by p.countryName
+
+INSERT into q4 
+Select p.countryName, Null as r0_2, Null as r2_4, Null as r4_6, count(*) as r6_8
+FROM parties_and_policy p
+WHERE  6 <= p.left_right  and p.left_right < 8
+Group by p.countryName 

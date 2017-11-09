4ef06a6d58f2cb7fd1820ec08291fddeffd78274
Anthony Wong <anthony_kc_wong@hotmail.com>
2017-11-08 22:39:53 -0500

    updated q3

--- table_definitions/q3.sql
+++ table_definitions/q3.sql
@@ -16,8 +16,19 @@ create table q3(
 DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
+create view intermediate_step as
+select c.name as name, date_part('year',e.e_date) as year, avg(e.votes_cast/e.electorate::float) as ratio
+from election e join country c on e.country_id = c.id
+where e.country_id = c.id and date_part('year',e.e_date) >= 2001 and date_part('year',e.e_date) <= 2016
+group by c.name, date_part('year',e.e_date);
 
 
 -- the answer to the query 
-insert into q3 
+insert into q3
+select name, year, ratio
+from intermediate_step
+where name not in (
+select i1.name
+from intermediate_step i1, intermediate_step i2
+where i1.name = i2.name and i1.year < i2.year and i1.ratio > i2.ratio);
 

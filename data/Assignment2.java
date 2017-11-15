7a6baa5a0c77e21cdcff829a6110d8044ff2a319
Le William <lewilli1@teach.cs.toronto.edu>
2017-11-15 11:50:36 -0500

    updated election sequence

--- data/Assignment2.java
+++ data/Assignment2.java
@@ -54,38 +54,38 @@ public class Assignment2 extends JDBCSubmission {
     public ElectionCabinetResult electionSequence(String countryName) {
         // Implement this method!
 	 	String queryString0, queryString1, queryString2, queryString3,queryString4,queryString5;
-		PreparedStatement pStatement;		    
 		ResultSet rs;
-		//just use arraylist for now
+		
 		List<Integer> elections = new ArrayList<>();
 		List<Integer> cabinets = new ArrayList<>();
 
     	JDBCSubmission.ElectionCabinetResult result = new JDBCSubmission.ElectionCabinetResult(elections, cabinets);
-		queryString0 = "DROP VIEW IF EXISTS all_elections CASCADE;"+
-						"DROP VIEW IF EXISTS all_cabinets CASCADE;"+
-						"DROP VIEW IF EXISTS p_elections CASCADE;"+
-						"DROP VIEW IF EXISTS e_elections CASCADE;";
-		queryString1 = "Create View all_elections as (" +
-						"SELECT c.name as countryName, c.id as countryId, e.id as electionId, e_date as electionDate," +
-						"e.e_type as eType, e.previous_parliament_election_id, e.previous_ep_election_id" +  
-						"FROM election e, country c" +
-						"WHERE e.country_id = c.id and c.name = ?);";
-		queryString2 = "Create View all_cabinets as (" +
-						"SELECT c2.name as countryName, c2.id as countryId, c1.start_date as startDate," +
-						" c1.previous_cabinet_id, c1.election_id, c1.id as cabinetId" +
-						"FROM cabinet c1, country c2" +
-						"WHERE c1.country_id = c2.id" +
-						"ORDER BY startDate desc);";
-		queryString3 = "Create view p_elections as (" +
-						"SELECT ae1.countryName, ae1.countryId, ae1.electionId,  ae1.electionDate, ae1.eType,"+
-						" ae2.previous_parliament_election_id as previousId, ae2.eType as nextType, ae2.electionDate as nextDate"+  
-						"FROM all_elections ae1 left join all_elections ae2 on ae2.previous_parliament_election_id = ae1.electionId and ae2.eType = ae1.eType"+
-						"WHERE ae1.eType = 'Parliamentary election');";
-		queryString4 = "Create view e_elections as (" +
-						"SELECT ae1.countryName, ae1.countryId, ae1.electionId,  ae1.electionDate, ae1.eType,"+ 
-						"ae2.previous_ep_election_id as previousId, ae2.eType as nexttype, ae2.electionDate  as nextdate"+
-						"FROM all_elections ae1 left join all_elections ae2 on ae2.previous_ep_election_id = ae1.electionId and ae2.eType = ae1.eType"+
-						"WHERE ae1.eType = 'European Parliament');"; 
+		
+		queryString0 = "DROP VIEW IF EXISTS all_elections CASCADE;\n"+
+						"DROP VIEW IF EXISTS all_cabinets CASCADE;\n"+
+						"DROP VIEW IF EXISTS p_elections CASCADE;\n"+
+						"DROP VIEW IF EXISTS e_elections CASCADE;\n";
+		queryString1 = "Create View all_elections as (\n" +
+						"SELECT c.name as countryName, c.id as countryId, e.id as electionId, e_date as electionDate,\n" +
+						"e.e_type as eType, e.previous_parliament_election_id, e.previous_ep_election_id\n" +  
+						"FROM election e, country c\n" +
+						"WHERE e.country_id = c.id);\n";
+		queryString2 = "Create View all_cabinets as (\n" +
+						"SELECT c2.name as countryName, c2.id as countryId, c1.start_date as startDate,\n" +
+						" c1.previous_cabinet_id, c1.election_id, c1.id as cabinetId\n" +
+						"FROM cabinet c1, country c2\n" +
+						"WHERE c1.country_id = c2.id\n" +
+						"ORDER BY startDate desc);\n";
+		queryString3 = "Create view p_elections as (\n" +
+						"SELECT ae1.countryName, ae1.countryId, ae1.electionId,  ae1.electionDate, ae1.eType,\n"+
+						" ae2.previous_parliament_election_id as previousId, ae2.eType as nextType, ae2.electionDate as nextDate\n"+  
+						"FROM all_elections ae1 left join all_elections ae2 on ae2.previous_parliament_election_id = ae1.electionId and ae2.eType = ae1.eType\n"+
+						"WHERE ae1.eType = 'Parliamentary election');\n";
+		queryString4 = "Create view e_elections as (\n" +
+						"SELECT ae1.countryName, ae1.countryId, ae1.electionId,  ae1.electionDate, ae1.eType,\n"+ 
+						"ae2.previous_ep_election_id as previousId, ae2.eType as nexttype, ae2.electionDate  as nextdate\n"+
+						"FROM all_elections ae1 left join all_elections ae2 on ae2.previous_ep_election_id = ae1.electionId and ae2.eType = ae1.eType\n"+
+						"WHERE ae1.eType = 'European Parliament');\n"; 
 		queryString5 = "Select * FROM\n" +
 						"(\n"+	
 
@@ -118,22 +118,35 @@ public class Assignment2 extends JDBCSubmission {
 						"from e_elections\n"+
 						"where nextDate is not null)a left join all_cabinets ap on a.countryName = ap.countryName and ap.startDate >= a.electionDate and  a.nextDate > ap.startDate\n"+
 
-						")result";
- 		System.out.println(queryString5);
+						")result WHERE result.countryName = ? Order By result.electionDate desc;";
+// 		System.out.println(queryString5);
+//		queryString6 = queryString0+queryString1+queryString2+queryString3+queryString4+queryString5;
+//		System.out.println(queryString6);
 		try {
-			PreparedStatement ps = super.connection.prepareStatement(queryString5);	
-//			ps.setString(1, countryName);
- 			rs = ps.executeQuery();
-		
-		
+			PreparedStatement ps0 = super.connection.prepareStatement(queryString0);	
+ 			ps0.executeUpdate();
+			PreparedStatement ps1 = super.connection.prepareStatement(queryString1);		
+			ps1.executeUpdate();
+			PreparedStatement ps2 = super.connection.prepareStatement(queryString2); 
+  			ps2.executeUpdate();
+			PreparedStatement ps3 = super.connection.prepareStatement(queryString3); 
+  			ps3.executeUpdate();
+			PreparedStatement ps4 = super.connection.prepareStatement(queryString4); 
+  			ps4.executeUpdate();
+			PreparedStatement ps5 = super.connection.prepareStatement(queryString5); 
+  			ps5.setString(1, String.valueOf(countryName));
+			rs = ps5.executeQuery();
+	
 			//get back stuff from execution	
 			 while (rs.next()) {
-				System.out.println("got inside");
-				int election_id = rs.getInt("electionId");
-				int cabinet_id = rs.getInt("cabinetid"); //do i have to check for null?
-				System.out.println(election_id);
-				result.elections.add(election_id);
-				result.cabinets.add(cabinet_id);
+				//System.out.println("got inside");
+				//int election_id = rs.getInt("electionId");
+				//int cabinet_id = rs.getInt("cabinetid"); //do i have to check for null?
+//				System.out.println(" election id: "+ election_id + " cabinet id: " + cabinet_id);
+	
+				//System.out.println(rs.getObject(7));
+				result.elections.add((Integer)rs.getObject(2));
+				result.cabinets.add((Integer)rs.getObject(7));
 			 }
 
     	}
@@ -143,6 +156,8 @@ public class Assignment2 extends JDBCSubmission {
 				"<Message>: " + se.getMessage());
 		}
 		System.out.println(result.toString());
+		System.out.println("length "+ result.elections.size() + "\n");
+		System.out.println("length "+ result.cabinets.size() + "\n");
 		return result;
 	}
 
@@ -167,7 +182,7 @@ public class Assignment2 extends JDBCSubmission {
 			System.out.println(a2.connectDB(url, name, password));
 	//		try {
   			  //some testing
-//            	a2.electionSequence("Canada");  
+            	a2.electionSequence("Japan");  
 
 
 			// queryString = "select* from party";

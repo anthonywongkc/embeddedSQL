e5df0e347e1da2b9ac37daf20b81e276e310f14f
Anthony Wong <anthony_kc_wong@hotmail.com>
2017-12-04 18:07:42 -0500

    added q2

---
+++ a3/q2.sql
@@ -0,0 +1,43 @@
+SET SEARCH_PATH to quizschema;
+DROP TABLE if exists q2 cascade;
+
+-- You must not change this table definition.
+
+CREATE TABLE q2(
+question_id INT,
+question_text VARCHAR(1000),
+numOfHints INT
+);
+
+-- You may find it convenient to do this for each of the views
+-- that define your intermediate steps.  (But give them better names!)
+DROP VIEW IF EXISTS question_tf CASCADE;
+DROP VIEW IF EXISTS question_mc CASCADE;
+DROP VIEW IF EXISTS question_num CASCADE;
+
+CREATE VIEW question_tf AS
+SELECT id AS question_id, description AS question_text
+FROM question JOIN true_false ON id = question_id;
+
+CREATE VIEW question_mc AS
+SELECT q.id AS question_id, q.description AS question_text, COUNT(hint) as numOfHints
+FROM question q, multiple_choice_opt o, hint_mc h
+WHERE q.id = o.question_id AND o.id = h.option_id
+GROUP BY q.id;
+
+CREATE VIEW question_num AS
+SELECT id AS question_id, description AS question_text, COUNT(hint) as numOfHints
+FROM question JOIN hint_num ON id = question_id
+GROUP BY id;
+
+INSERT INTO q2
+SELECT question_id, question_text, NULL
+FROM question_tf;
+
+INSERT INTO q2
+SELECT *
+FROM question_mc;
+
+INSERT INTO q2
+SELECT *
+FROM question_num;

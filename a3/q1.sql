6c5d5bfab0f31b21ff0d634579977c57a1d89bba
Le William <lewilli1@teach.cs.toronto.edu>
2017-12-06 12:20:38 -0500

    update q1

--- a3/q1.sql
+++ a3/q1.sql
@@ -1,7 +1,7 @@
 -- Report the full name and student number of all students in the database
 
 SET SEARCH_PATH TO quizschema;
-drop table if exists q1 cascade;
+--drop table if exists q1 cascade;
 
 SELECT first_name || ' ' || last_name AS full_name, id AS student_number
 FROM student;
